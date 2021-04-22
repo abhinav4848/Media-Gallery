99119257a6b272d4e2e253fa7b97e9f9cd0dfa34
Abhinav <abhinav4848@gmail.com>
2021-04-22 05:47:19 +0530

    another db backup

---
+++ database 2 (try if first one fails).sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2021 at 02:16 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spsdaurm_users`
+--
+CREATE DATABASE IF NOT EXISTS `spsdaurm_users` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `spsdaurm_users`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_comments`
+--
+
+CREATE TABLE `media_comments` (
+  `id` int(11) NOT NULL,
+  `media_comment` text NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_entries`
+--
+
+CREATE TABLE `media_entries` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `filename_final` text NOT NULL,
+  `filename_original_name` text NOT NULL,
+  `filename_ext` text NOT NULL,
+  `hash` text NOT NULL,
+  `similar_hash` text NOT NULL,
+  `upload_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_list_of_tags`
+--
+
+CREATE TABLE `media_list_of_tags` (
+  `id` int(11) NOT NULL,
+  `tag_name` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_ratings`
+--
+
+CREATE TABLE `media_ratings` (
+  `id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `media_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media_tag`
+--
+
+CREATE TABLE `media_tag` (
+  `id` int(11) NOT NULL,
+  `media_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `media_comments`
+--
+ALTER TABLE `media_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `media_entries`
+--
+ALTER TABLE `media_entries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `media_list_of_tags`
+--
+ALTER TABLE `media_list_of_tags`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `media_ratings`
+--
+ALTER TABLE `media_ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `media_tag`
+--
+ALTER TABLE `media_tag`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `media_comments`
+--
+ALTER TABLE `media_comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media_entries`
+--
+ALTER TABLE `media_entries`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media_list_of_tags`
+--
+ALTER TABLE `media_list_of_tags`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media_ratings`
+--
+ALTER TABLE `media_ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `media_tag`
+--
+ALTER TABLE `media_tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
